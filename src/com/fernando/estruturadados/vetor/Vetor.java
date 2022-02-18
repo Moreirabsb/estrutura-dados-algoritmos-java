a41ea0999e377eb89f1be799a041e8563fc9e2c5
Fernando de Paula <fernandodepaulabsb@gmail.com>
2022-02-17 23:21:49 -0300

    Inicio classe Vetor

---
+++ src/com/fernando/estruturadados/vetor/Vetor.java
@@ -0,0 +1,12 @@
+package com.fernando.estruturadados.vetor;
+
+public class Vetor {
+	private String[] elementos;
+
+	public Vetor(Integer capacidade) {
+		this.elementos = new String[capacidade];
+	}
+	
+	
+
+}
